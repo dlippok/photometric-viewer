6cc4dbc9293ab8deacc9090fe65110de5c58944b
DaLee <mail.dalee@gmail.com>
2023-05-01 18:45:07 +0200

    Display general information on top

--- photometric_viewer/gui/general.py
+++ photometric_viewer/gui/general.py
@@ -2,12 +2,13 @@ from photometric_viewer.gui.widgets.common import PropertyList
 from photometric_viewer.model.photometry import Photometry
 
 
-class LuminaireProperties(PropertyList):
+class GeneralLuminaireInformation(PropertyList):
     def set_photometry(self, photometry: Photometry):
         items = [i for i in self]
 
         for child in items:
             self.remove(child)
 
-        for key, value in photometry.metadata.additional_properties.items():
-            self.append(self._create_item(name=key, value=value))
+        self.append(self._create_item(name="Name", value=photometry.metadata.luminaire))
+        self.append(self._create_item(name="Manufacturer", value=photometry.metadata.manufacturer))
+
