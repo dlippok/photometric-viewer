9fed7c1e3e92282563744b48243c664ceff66116
DaLee <mail.dalee@gmail.com>
2023-05-01 18:04:11 +0200

    Display source code of the selected file

--- photometric_viewer/gui/content.py
+++ photometric_viewer/gui/content.py
@@ -1,5 +1,5 @@
-from gi.repository import Gtk
-from gi.repository.Gtk import Orientation
+from gi.repository import Gtk, Adw
+from gi.repository.Gtk import Orientation, PolicyType, ScrolledWindow
 
 from photometric_viewer.gui.geometry import LuminaireGeometry
 from photometric_viewer.gui.lamps import LampAndBallast
@@ -9,7 +9,7 @@ from photometric_viewer.gui.header import Header
 from photometric_viewer.gui.properties import LuminaireProperties
 
 
-class PhotometryContent(Gtk.Box):
+class PhotometryContent(Adw.Bin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.diagram = PhotometricDiagram()
@@ -17,24 +17,35 @@ class PhotometryContent(Gtk.Box):
         self.geometry = LuminaireGeometry()
         self.lamps_and_ballast = LampAndBallast()
 
-        self.set_orientation(Orientation.VERTICAL)
-        self.set_spacing(16)
-        self.set_margin_top(32)
-        self.set_margin_bottom(32)
-        self.set_margin_start(16)
-        self.set_margin_end(16)
+        box = Gtk.Box(
+            orientation=Orientation.VERTICAL,
+            spacing=16,
+            margin_top=32,
+            margin_bottom=32,
+            margin_start=32,
+            margin_end=32
+        )
 
-        self.append(Header(label="Photometric diagram", xalign=0))
-        self.append(self.diagram)
+        box.append(Header(label="Photometric diagram", xalign=0))
+        box.append(self.diagram)
 
-        self.append(Header(label="Geometry", xalign=0))
-        self.append(self.geometry)
+        box.append(Header(label="Geometry", xalign=0))
+        box.append(self.geometry)
 
-        self.append(Header(label="Lamps and ballast", xalign=0))
-        self.append(self.lamps_and_ballast)
+        box.append(Header(label="Lamps and ballast", xalign=0))
+        box.append(self.lamps_and_ballast)
 
-        self.append(Header(label="Properties", xalign=0))
-        self.append(self.properties)
+        box.append(Header(label="Properties", xalign=0))
+        box.append(self.properties)
+
+        clamp = Adw.Clamp()
+        clamp.set_child(box)
+
+        scrolled_window = ScrolledWindow()
+        scrolled_window.set_child(clamp)
+        scrolled_window.set_vexpand(True)
+        scrolled_window.set_policy(PolicyType.NEVER, PolicyType.AUTOMATIC)
+        self.set_child(scrolled_window)
 
     def set_photometry(self, photometry: Photometry):
         self.diagram.set_photometry(photometry)
