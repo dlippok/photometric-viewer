01db3e750715edd66e55b152a964a57e72ead222
DaLee <mail.dalee@gmail.com>
2023-05-19 11:20:34 +0200

    Add about dialog

---
+++ photometric_viewer/gui/about.py
@@ -0,0 +1,19 @@
+from gi.repository import Adw, Gtk
+
+
+class AboutWindow():
+    def __init__(self):
+        self._about_window = Adw.AboutWindow(
+            application_name="Photomertic Viewer",
+            application_icon="io.github.dlippok.photometric-viewer",
+            developer_name="Damian Lippok",
+            version="0.2",
+            website="https://github.com/dlippok/photometric-viewer",
+            issue_url="https://github.com/dlippok/photometric-viewer/issues",
+            copyright="© 2023 Damian Lippok",
+            license_type=Gtk.License.MIT_X11
+        )
+
+
+    def show(self):
+        self._about_window.show()
---
+++ photometric_viewer/gui/menu.py
@@ -0,0 +1,25 @@
+from gi.repository import Gtk
+
+
+class ApplicationMenuButton(Gtk.MenuButton):
+    MENU_MODEL = """
+    <?xml version="1.0"?>
+        <interface>
+        <menu id='app-menu'>
+            <section>
+                <item>
+                    <attribute name='label' translatable='yes'>About Photometric Viewer</attribute>
+                    <attribute name='action'>app.show_about_window</attribute>
+                </item>
+            </section>
+        </menu>
+        </interface>
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        builder = Gtk.Builder.new_from_string(self.MENU_MODEL, -1)
+        menu_model = builder.get_object("app-menu")
+        self.set_menu_model(menu_model)
+        self.set_icon_name("open-menu-symbolic")
