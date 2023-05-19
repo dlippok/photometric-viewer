32ca3d056ce52e0eac3f34759abfbbaeeae5cb7b
DaLee <mail.dalee@gmail.com>
2023-05-19 10:32:44 +0200

    Do not ellipsize long values

--- photometric_viewer/gui/widgets/common.py
+++ photometric_viewer/gui/widgets/common.py
@@ -10,7 +10,7 @@ class PropertyList(Gtk.ListBox):
         self.set_css_classes(["boxed-list"])
         self.set_selection_mode(SelectionMode.NONE)
 
-    def _create_item(self, name, value:str, wrap=False):
+    def _create_item(self, name, value: str):
         box = Box(orientation=Orientation.VERTICAL)
         box.set_homogeneous(False)
         box.set_spacing(4)
@@ -22,23 +22,16 @@ class PropertyList(Gtk.ListBox):
         name_label = Label(label=name, hexpand=True, xalign=0)
         name_label.set_css_classes(["h1"])
 
-        displayed_value = value.replace("\n", " ") if wrap else value
-
         value_label = Label(
-            label=displayed_value or "Unknown",
+            label=value or "Unknown",
             tooltip_text=value,
             hexpand=True,
             xalign=0,
             selectable=True,
-            css_classes=[] if value else ["warning"]
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            css_classes=[] if value else ["warning"],
         )
-
-        if wrap:
-            value_label.set_wrap(True)
-            value_label.set_wrap_mode(WrapMode.WORD_CHAR)
-        else:
-            value_label.set_ellipsize(EllipsizeMode.END)
-            value_label.set_width_chars(16)
 
         box.append(name_label)
         box.append(value_label)
