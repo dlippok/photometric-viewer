306abd8d02177c6a2d5f36128e4d64e72e60070a
DaLee <mail.dalee@gmail.com>
2023-05-19 10:24:15 +0200

    Make value labels selectable

--- photometric_viewer/gui/widgets/common.py
+++ photometric_viewer/gui/widgets/common.py
@@ -1,7 +1,6 @@
 from gi.repository import Gtk
-from gi.repository.GObject import Property
 from gi.repository.Gdk import Clipboard, ContentProvider
-from gi.repository.Gtk import Box, Orientation, Label, SelectionMode, Button
+from gi.repository.Gtk import Box, Orientation, Label, SelectionMode
 from gi.repository.Pango import EllipsizeMode, WrapMode
 
 
@@ -27,11 +26,12 @@ class PropertyList(Gtk.ListBox):
 
         value_label = Label(
             label=displayed_value or "Unknown",
+            tooltip_text=value,
+            hexpand=True,
+            xalign=0,
+            selectable=True,
             css_classes=[] if value else ["warning"]
         )
-        value_label.set_tooltip_text(value)
-        value_label.set_hexpand(True)
-        value_label.set_xalign(0)
 
         if wrap:
             value_label.set_wrap(True)
@@ -40,16 +40,8 @@ class PropertyList(Gtk.ListBox):
             value_label.set_ellipsize(EllipsizeMode.END)
             value_label.set_width_chars(16)
 
-        value_box = Box(orientation=Orientation.HORIZONTAL, spacing=16)
-        value_box.append(value_label)
-
-        if value:
-            copy_button: Button = Button.new_from_icon_name("edit-copy")
-            copy_button.connect("clicked", self.on_copy_clicked, value)
-            value_box.append(copy_button)
-
         box.append(name_label)
-        box.append(value_box)
+        box.append(value_label)
 
         return box
 
