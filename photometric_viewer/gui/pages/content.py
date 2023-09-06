b387a0e3e1f0e5993a211633313c1417e7bd7189
Damian Lippok <mail.dalee@gmail.com>
2023-09-06 23:26:34 +0200

    Use compact header with basic luminaire data

--- photometric_viewer/gui/pages/content.py
+++ photometric_viewer/gui/pages/content.py
@@ -1,22 +1,20 @@
 from gi.repository import Gtk, Adw
 from gi.repository.Gtk import Orientation, PolicyType, ScrolledWindow
 
-from photometric_viewer.gui.widgets.content.general import GeneralLuminaireInformation
+from photometric_viewer.gui.widgets.common.header import Header
 from photometric_viewer.gui.widgets.content.geometry import LuminaireGeometryProperties
+from photometric_viewer.gui.widgets.content.header import LuminaireHeader
 from photometric_viewer.gui.widgets.content.lamps import LampAndBallast
 from photometric_viewer.gui.widgets.content.photometry import LuminairePhotometricProperties
-from photometric_viewer.model.photometry import Photometry
-from photometric_viewer.gui.widgets.content.diagram import PhotometricDiagram
-from photometric_viewer.gui.widgets.common.header import Header
 from photometric_viewer.gui.widgets.content.properties import LuminaireProperties
+from photometric_viewer.model.photometry import Photometry
 from photometric_viewer.model.settings import Settings
 
 
 class PhotometryContent(Adw.Bin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.diagram = PhotometricDiagram()
-        self.general_information = GeneralLuminaireInformation()
+        self.header = LuminaireHeader()
         self.photometric_properties = LuminairePhotometricProperties()
         self.geometry = LuminaireGeometryProperties()
         self.lamps_and_ballast = LampAndBallast()
@@ -25,18 +23,13 @@ class PhotometryContent(Adw.Bin):
         box = Gtk.Box(
             orientation=Orientation.VERTICAL,
             spacing=16,
-            margin_top=32,
-            margin_bottom=32,
-            margin_start=32,
-            margin_end=32
+            margin_top=16,
+            margin_bottom=16,
+            margin_start=16,
+            margin_end=16
         )
 
-        box.append(Header(label=_("Photometric diagram"), xalign=0))
-        box.append(self.diagram)
-
-        box.append(Header(label=_("General information"), xalign=0))
-        box.append(self.general_information)
-
+        box.append(self.header)
         box.append(self.photometric_properties)
 
         box.append(Header(label=_("Geometry"), xalign=0))
@@ -47,7 +40,7 @@ class PhotometryContent(Adw.Bin):
 
         box.append(self.properties)
 
-        clamp = Adw.Clamp()
+        clamp = Adw.Clamp(maximum_size=800)
         clamp.set_child(box)
 
         scrolled_window = ScrolledWindow()
@@ -57,14 +50,13 @@ class PhotometryContent(Adw.Bin):
         self.set_child(scrolled_window)
 
     def set_photometry(self, photometry: Photometry):
-        self.diagram.set_photometry(photometry)
-        self.general_information.set_photometry(photometry)
+        self.header.set_photometry(photometry)
         self.photometric_properties.set_photometry(photometry)
         self.geometry.set_photometry(photometry)
         self.lamps_and_ballast.set_photometry(photometry)
         self.properties.set_photometry(photometry)
 
     def update_settings(self, settings: Settings):
-        self.diagram.update_settings(settings)
+        self.header.update_settings(settings)
         self.lamps_and_ballast.update_settings(settings)
         self.geometry.update_settings(settings)
---
+++ photometric_viewer/gui/widgets/content/header.py
@@ -0,0 +1,89 @@
+from gi.repository.Gtk import Box, Orientation, Label, Align
+from gi.repository.Pango import WrapMode
+
+from photometric_viewer.gui.widgets.content.diagram import PhotometricDiagram
+from photometric_viewer.model.photometry import Photometry
+from photometric_viewer.model.settings import Settings
+
+
+class LuminaireHeader(Box):
+    def __init__(self):
+        super().__init__(
+            orientation=Orientation.HORIZONTAL,
+            spacing=16,
+        )
+
+        self.set_homogeneous(True)
+        self.diagram = PhotometricDiagram(
+            vexpand=False,
+            valign=Align.START,
+            content_width=150
+        )
+
+        self.name_label = Label(
+            xalign=0,
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            selectable=True,
+
+            css_classes=["title-3"]
+
+        )
+        self.manufacturer_label = Label(
+            xalign=0,
+            hexpand=True,
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            selectable=True,
+            css_classes=["title-4"]
+        )
+        self.description_label = Label(
+            xalign=0,
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            selectable=True
+        )
+        self.date_label = Label(
+            xalign=0,
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            selectable=True,
+            css_classes=["dim-label"]
+        )
+
+        self.measurement_label = Label(
+            xalign=0,
+            wrap=True,
+            wrap_mode=WrapMode.WORD_CHAR,
+            selectable=True,
+            css_classes=["dim-label"]
+        )
+
+        properties_box = Box(orientation=Orientation.VERTICAL, spacing=12)
+        properties_box.append(self.name_label)
+        properties_box.append(self.manufacturer_label)
+        properties_box.append(self.date_label)
+        properties_box.append(self.measurement_label)
+        properties_box.append(self.description_label)
+
+        self.append(self.diagram)
+        self.append(properties_box)
+
+    def set_photometry(self, photometry: Photometry):
+        self.name_label.set_label(photometry.metadata.catalog_number or "")
+        self.manufacturer_label.set_label(photometry.metadata.manufacturer or "")
+        self.description_label.set_label(photometry.metadata.luminaire or "")
+        self.date_label.set_label(photometry.metadata.date_and_user or "")
+
+        if photometry.metadata.measurement:
+            self.measurement_label.set_label(f'{_("Measurement")}: {photometry.metadata.measurement}')
+            self.measurement_label.set_visible(True)
+        else:
+            self.measurement_label.set_label("")
+            self.measurement_label.set_visible(False)
+
+        self.diagram.set_photometry(photometry)
+
+    def update_settings(self, settings: Settings):
+        self.diagram.update_settings(settings)
+
