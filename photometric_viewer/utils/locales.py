df2ded999833287b336b2f6aed64e30acfbad426
DaLee <mail.dalee@gmail.com>
2023-10-02 21:18:50 +0200

    Fix monetary locale when running as SNAP package

---
+++ photometric_viewer/utils/locales.py
@@ -0,0 +1,23 @@
+import gettext
+import locale
+import os
+
+
+def init_locale(application_id):
+    locale_dir = "data/translations"
+    locale.setlocale(locale.LC_ALL, '')
+
+    if os.environ.get("container") == "flatpak":
+        locale_dir = "/app/share/locale"
+
+    elif root := os.environ.get("SNAP") and os.environ.get("SNAP_INSTANCE_NAME") == "photometric-viewer":
+        locale_dir = root + "/share/locale"
+        try:
+            normalized_monetary = locale.normalize(locale.getlocale(locale.LC_MONETARY)[0])
+            print(normalized_monetary)
+            locale.setlocale(locale.LC_MONETARY, normalized_monetary)
+        except locale.Error:
+            pass
+
+    locale.bindtextdomain(application_id, locale_dir)
+    gettext.install(application_id, locale_dir)
