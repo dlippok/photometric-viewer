e7af8a1c3fb223ec9669c568dfd33a51b10b45f8
Damian Lippok <mail.dalee@gmail.com>
2024-02-18 20:10:53 +0100

    Import IESNA LM-63-02 correctly

--- photometric_viewer/formats/common.py
+++ photometric_viewer/formats/common.py
@@ -17,7 +17,7 @@ def import_from_file(f: IO):
         photometry = ies02_converter.convert_content(content)
         f.seek(0)
         photometry.metadata.file_source = f.read()
-    if possible_ies_header and possible_ies_header.upper().startswith("IESNA"):
+    elif possible_ies_header and possible_ies_header.upper().startswith("IESNA"):
         content = ies95_extractor.extract_content(f)
         photometry = ies95_converter.convert_content(content)
         f.seek(0)
